ca9bc6fa97edf5d34d0587fb50f11bb1d8fb7be3
Deekshu Gudla <deekshugudla2@gmail.com>
2024-11-26 11:01:19 +0530

    created multipilcation class

---
+++ src/main/java/com/gvp/calc/Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*
+class Multipilcation
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a num:");
+        double a=sc.nextDouble();
+        System.out.println("enter a num:");
+        double b=sc.nextDouble();  
+        System.out.println("product is: ",a*b);
+        sc.close();
+     }
+}
