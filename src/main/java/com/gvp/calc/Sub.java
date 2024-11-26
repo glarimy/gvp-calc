b34a9062a0dbd3fd0319c44bbdf22f1878de0a6c
KOVIRI DEEPIKA <kovirideepika@gmail.com>
2024-11-26 11:20:17 +0530

    create Sub class

---
+++ src/main/java/com/gvp/calc/Sub.java
@@ -0,0 +1,10 @@
+package com.gvp.calc;
+
+public class Sub {
+    public static void main(String[] args) {
+        int num1 = 10; // First number
+        int num2 = 4;  // Second number
+        int result = num1 - num2; // Perform subtraction
+        System.out.println("The result is: " + result);
+    }
+}
