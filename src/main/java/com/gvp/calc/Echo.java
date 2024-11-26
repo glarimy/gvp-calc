ed5534a4315924eeb00c9a61f0e86a492c674e01
farhathashmi1413@gmail.com <farhathashmi1413@gmail.com>
2024-11-26 11:06:38 +0530

    created mod op

---
+++ src/main/java/com/gvp/calc/Echo.java
@@ -0,0 +1,10 @@
+public class Echo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int result = a % b;
+
+        System.out.println("The modulus of " + a + " % " + b + " is: " + result);
+    }
+}
