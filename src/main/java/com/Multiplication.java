d7c6b4e26881745fcccf57ba6f50d7be73f26d90
Deekshu Gudla <deekshugudla2@gmail.com>
2024-11-26 11:32:38 +0530

    multliplication

--- src/main/java/com/Multiplication.java
+++ src/main/java/com/Multiplication.java
@@ -1,14 +1,10 @@
 import java.util.*
 class Multipilcation
 {
-    public static void main(String[] args)
+    public int Multiplication(int num1,int num2)
     {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter a num:");
-        double a=sc.nextDouble();
-        System.out.println("enter a num:");
-        double b=sc.nextDouble();  
-        System.out.println("product is: ",a*b);
-        sc.close();
-     }
+        return num1*num2;
+    }
+        
+     
 }
